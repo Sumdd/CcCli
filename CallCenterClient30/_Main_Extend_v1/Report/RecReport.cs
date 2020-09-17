c8ba17103124efe61de592ad85e1e523dba11d24
Administrator <Administrator@DESKTOP-3VP0P8Q>
2020-09-17 17:35:19 +0800

    2020.09.17 王卫东 提交 修改统计表的超时时间为1小时

--- CallCenterClient30/_Main_Extend_v1/Report/RecReport.cs
+++ CallCenterClient30/_Main_Extend_v1/Report/RecReport.cs
@@ -287,7 +287,7 @@ namespace CenoCC
                     this.qop.existQuery("endSpeakTime");
                     //部门
                     this.qop.existQuery("team");
-                    DataSet ds = this.qop.QdataSet();
+                    DataSet ds = this.qop.QdataSet(60 * 60);
                     //缓存当页数据,以便进行当前页录音下载
                     if (this.m_ds != null)
                     {
